09d60be6767e205e4b6b532a4ad3301bec6effcc
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-10-30 15:29:59 +0900

    Initial Commit

---
+++ MapAppPart2/MapAppPart2App.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppPart2App.swift
+//  MapAppPart2
+//
+//  Created by 渡邊魁優 on 2023/10/30.
+//
+
+import SwiftUI
+
+@main
+struct MapAppPart2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
